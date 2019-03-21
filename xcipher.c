e495e3486b1b571f16b2ebd6474e1d27c1a7ad4d
everythng <48783361+everythng@users.noreply.github.com>
2019-03-21 03:09:31 -0300

    ...

---
+++ xcipher.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *xor_encrypt(char *msg, char *key){
+     int len = strlen(msg), j=0;
+     char *ptr = malloc(len+1);
+     
+     while(j<(len)){
+          ptr[j++] = (*msg++)^key[j%len];
+     }         
+     
+     return ptr;
+}
+
+int main(int argc, char *argv[]){
+    char *msg = "sct-msg";
+    char *pr = xor_encrypt(msg, argv[1]);
+    
+    printf("%-5s\n", pr);
+    free(pr);
+    return 0x0;
+}
