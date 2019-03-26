acdf459a4dc88a2fc9bc4f13b0f476d6fcd53a91
everythng <48783361+everythng@users.noreply.github.com>
2019-03-26 12:28:01 -0300

    Create count.asm

---
+++ count.asm
@@ -0,0 +1,51 @@
+%macro exit 0
+       mov eax, 1
+       xor ebx, ebx
+       int 0x80
+%endmacro
+
+section .rodata
+        n db 0xa
+        
+        
+%macro write_nl 0
+       mov eax, 4
+       mov ebx, 1
+       mov edx, 1
+       mov ecx, n
+       int 0x80
+%endmacro
+
+section .bss
+        num resb 10
+
+section .text
+         global _start
+         
+_start:
+         xor ecx,ecx
+         
+          .loop:
+          
+                 inc ecx
+                 add ecx, '0'
+                 
+                 mov [num], ecx
+                 push ecx
+                
+                 mov eax, 4
+                 mov ebx, 1
+                 mov edx, 1
+                 mov ecx, num
+                 
+                 int 0x80
+                 
+                 pop ecx
+                 sub ecx, '0'
+                               
+                 cmp ecx, 9
+                 jne .loop
+                      
+           write_nl
+           exit
+      
